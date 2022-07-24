dd26890483428e661b3c2736f7ea6f343b6e733b
Montagab Al-Hamawy <ma0152260@gmail.com>
2022-07-24 08:12:57 +0300

    Update Node.js

--- js/Node.js
+++ js/Node.js
@@ -1,1 +1,33 @@
-var x = parseInt( readline() );
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding( "utf-8" );
+
+function print ( x )
+{
+	console.log( x );
+}
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on( "data", ( inputStdin ) =>
+{
+	inputString += inputStdin;
+} );
+process.stdin.on( "end", () =>
+{
+	inputString = inputString.split( "\n" );
+	main();
+} );
+function readline ()
+{
+	return inputString[ currentLine++ ];
+}
+
+// ********** Code Start **********
+
+function main ()
+{
+	// cin
+	var x = parseInt( readline() );
+	
+}
